2b0ecea3b5cd3c1c403252791e5b0f556edecdd3
nathan choi <nath49ers@gmail.com>
2023-06-17 14:35:48 -0700

    new things

---
+++ Lectern/Backend/Content/ContentManagerStudy.swift
@@ -0,0 +1,8 @@
+//
+//  ContentManagerStudy.swift
+//  Lectern
+//
+//  Created by Nathan Choi on 6/17/23.
+//
+
+import SwiftUI
--- Lectern/ContentView.swift
+++ Lectern/ContentView.swift
@@ -15,29 +15,29 @@ struct ContentView: View {
     @State var mainText = [String]()
 
     var body: some View {
-//        ParentView()
-        HStack(spacing: 5) {
-            ForEach(mainText.indices, id:\.self) { i in
-                LecternTextReveal(word: mainText[i])
-                    .animation(.spring)
-                    .id(i)
-            }
-        }
-        .onChange(of: sr.transcript) {
-            mainText = sr.transcript.components(separatedBy: " ")
-        }
-                .transition(.scale)
-                .animation(.spring, value: sr.transcript)
-                .environment(nvm)
-                .task {
-                    print("STARTIN SCRIPT")
-                    sr.resetTranscript()
-                    sr.startTranscribing()
-
-                    //try? await Task.sleep(nanoseconds: 5_000_000_000)
-                    //sr.stopTranscribing()
-                    //print(sr.transcript)
-                }
+        ParentView()
+//        HStack(spacing: 5) {
+//            ForEach(mainText.indices, id:\.self) { i in
+//                LecternTextReveal(word: mainText[i])
+//                    .animation(.spring)
+//                    .id(i)
+//            }
+//        }
+//        .onChange(of: sr.transcript) {
+//            mainText = sr.transcript.components(separatedBy: " ")
+//        }
+//                .transition(.scale)
+//                .animation(.spring, value: sr.transcript)
+//                .environment(nvm)
+//                .task {
+//                    print("STARTIN SCRIPT")
+//                    sr.resetTranscript()
+//                    sr.startTranscribing()
+//
+//                    //try? await Task.sleep(nanoseconds: 5_000_000_000)
+//                    //sr.stopTranscribing()
+//                    //print(sr.transcript)
+//                }
     }
 }
 
