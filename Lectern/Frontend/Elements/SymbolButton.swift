6b30954fbdce5b178cb2389faa57d485fd11bb5b
polarizz <polarizz39@gmail.com>
2023-06-17 12:27:41 -0700

    .

--- Lectern/Frontend/Elements/SymbolButton.swift
+++ Lectern/Frontend/Elements/SymbolButton.swift
@@ -16,7 +16,7 @@ struct SymbolButton: View {
     var fill: Color
     var action: (() -> Void)?
 
-    init(symbol: String, color: Color = Color.main, fill: Color = Color.clear, action: (() -> Void)?=nil) {
+    init(symbol: String, color: Color = .main, fill: Color = Color.clear, action: (() -> Void)?=nil) {
         self.symbol = symbol
         self.color = color
         self.fill = fill
@@ -31,28 +31,6 @@ struct SymbolButton: View {
                 .padding(sizeClass == .compact ? 13 : 15)
                 .background(fill)
                 .clipShape(RoundedRectangle(cornerRadius: 9, style: .continuous))
-                .contentShape(Rectangle())
-                .hoverEffect(.highlight)
-        }
-    }
-}
-
-struct SymbolButtonSelection: View {
-
-    @Environment(\.horizontalSizeClass) var sizeClass
-    @Environment(\.colorScheme) var colorScheme
-
-    var symbol: String
-    var selected: Bool
-    var action: (() -> Void)?
-
-    var body: some View {
-        Button(action: { action?() }) {
-            Image(systemName: symbol)
-                .font(.system(size: UIConstants.body).weight(.medium))
-                .frame(width: 19, height: 19)
-                .padding(sizeClass == .compact ? 13 : 15)
-                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
                 .contentShape(Rectangle())
                 .hoverEffect(.highlight)
         }
