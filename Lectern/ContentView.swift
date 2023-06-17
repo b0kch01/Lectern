3245cfd5921c4a855aeffac8daa78359b46d3178
nathan choi <nath49ers@gmail.com>
2023-06-17 10:36:50 -0700

    initial project creation

---
+++ Lectern/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lectern
+//
+//  Created by Nathan Choi on 6/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lectern/LecternApp.swift
@@ -0,0 +1,17 @@
+//
+//  LecternApp.swift
+//  Lectern
+//
+//  Created by Nathan Choi on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LecternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
