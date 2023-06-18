2481ab8899dce5efba0b0b9213a7497db629abc1
nathan choi <nath49ers@gmail.com>
2023-06-17 23:18:47 -0700

    new things

--- Lectern/Backend/Content/ContentManagerStudy.swift
+++ Lectern/Backend/Content/ContentManagerStudy.swift
@@ -7,10 +7,23 @@
 
 import SwiftUI
 
+struct GPTResponse: Codable {
+    let role: String
+    let content: String
+}
+
 extension ContentManager {
 
     func blurt() {
-        
-    }
+        let url = URL(string: URL_K.blurt)!
+        let req = URLRequest(url: url)
 
+        Task {
+            let _, data = try? await URLSession().data(for: req)
+
+            guard let json = JSONDecoder().decode(GPTResponse.self, from: data) else { return }
+
+            logger.info(json)
+        }
+    }
 }
