c01fc1b8a97c2c02cd02c1bed5745bfee3d59b91
polarizz <polarizz39@gmail.com>
2023-06-22 22:16:56 -0700

    Push

--- Lectern/STIView.swift
+++ Lectern/STIView.swift
@@ -15,9 +15,9 @@ struct STIView: View {
 
     @State var fake: CGFloat = 0.0
 
-    @State var primaryColor: Color = .blue
-    @State var secondaryColor: Color = .cyan
-    @State var tertiaryColor: Color = .mint
+    @State var primaryColor: Color = .blaze
+    @State var secondaryColor: Color = .blaze
+    @State var tertiaryColor: Color = .yellow
 
     @State var showPopup = false
     @State var showCalendar = false
@@ -26,6 +26,10 @@ struct STIView: View {
     @State private var image = UIImage()
 
     @State var date: String = "January 1"
+
+    @State var bounced = true
+
+    let timer = Timer.publish(every: 0.9, on: .main, in: .common).autoconnect()
 
     var body: some View {
         TabView {
@@ -52,8 +56,7 @@ struct STIView: View {
                     .padding(.horizontal)
                     .submitLabel(.done)
                 }
-                .padding(.vertical)
-                .padding(.horizontal, 30)
+                .padding()
 
                 Spacer()
             }
@@ -130,23 +133,6 @@ struct STIView: View {
                 }
 
                 Button(action: {
-                    withAnimation(.smooth) {
-                        showCamera = true
-                    }
-                }) {
-                    Image(systemName: "camera.viewfinder")
-                        .font(.system(size: 21))
-                        .frame(width: 24, height: 24)
-                        .padding(11)
-                        .background(.white.opacity(0.15))
-                        .clipShape(Circle())
-                }
-                .fullScreenCover(isPresented: $showCamera) {
-                    ImagePicker(sourceType: .camera, selectedImage: self.$image)
-                        .background(Color.black.ignoresSafeArea())
-                }
-
-                Button(action: {
 
                 }) {
                     Image(systemName: "ellipsis")
@@ -174,9 +160,38 @@ struct STIView: View {
             .foregroundStyle(.white.opacity(0.9))
             .opacity(showCalendar ? 0 : 1)
 
+            ZStack {
+                Image(systemName: "figure.arms.open")
+                    .font(.system(size: 439))
+                    .foregroundStyle(.white.opacity(0.3))
+
+                Image(systemName: "heart.fill")
+                    .font(.system(size: 24))
+                    .foregroundStyle(.red.opacity(0.7))
+                    .offset(x: 10, y: -60)
+                    .blur(radius: 2)
+                    .symbolEffect(.bounce, value: bounced)
+                    .onReceive(timer) { _ in
+                        bounced.toggle()
+                    }
+
+                circle
+                    .offset(x: 0, y: 60)
+
+                circle
+                    .offset(x: 0, y: -160)
+
+                circle
+                    .offset(x: 59, y: 160)
+
+                circle
+                    .offset(x: -107, y: -10)
+            }
+            .padding(.horizontal, 40)
+
             VStack(spacing: 0) {
                 LeadingHStack {
-                    Text("Do you have a fever or other flu-like symptoms?")
+                    Text("What symptoms are you experiencing around your mouth?")
                         .font(.title.weight(.bold))
                 }
                 .padding(.trailing)
@@ -185,31 +200,44 @@ struct STIView: View {
                     VStack(spacing: 5) {
                         Group {
                             TrailingHStack {
-                                Text("Yes")
+                                Text("Sores")
                                     .font(.title2.weight(.bold))
                                     .multilineTextAlignment(.trailing)
                             }
                             .padding(.leading)
-                            .padding(13)
+                            .padding(.vertical, 9)
+                            .padding(.horizontal, 13)
                             .background(.white.opacity(0.15))
                             .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
                             
                             TrailingHStack {
-                                Text("No")
+                                Text("Rashes")
                                     .font(.title2.weight(.bold))
                                     .multilineTextAlignment(.trailing)
                             }
                             .padding(.leading)
-                            .padding(13)
+                            .padding(.vertical, 9)
+                            .padding(.horizontal, 13)
                             .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
-                            
-                            Bar()
+
+                            TrailingHStack {
+                                Text("Itching")
+                                    .font(.title2.weight(.bold))
+                                    .multilineTextAlignment(.trailing)
+                            }
+                            .padding(.leading)
+                            .padding(.vertical, 9)
+                            .padding(.horizontal, 13)
+                            .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
+
+                            Bar(color: .white.opacity(0.3))
                                 .padding(.vertical)
                             
                             TrailingHStack {
-                                Text("Anything else to add?")
+                                Text("Describe any symptoms not listed above. For example, pus leakage, bleeding.")
                                     .font(.title2.weight(.bold))
                                     .multilineTextAlignment(.trailing)
+                                    .opacity(0.5 )
                             }
                             .padding(.leading)
                         }
@@ -229,8 +257,8 @@ struct STIView: View {
             }
             .foregroundStyle(.white)
             .padding(.top, 120)
-            .padding(.vertical)
             .padding(.horizontal, 30)
+            .padding()
         }
         .background(
             FluidGradient(
@@ -249,9 +277,9 @@ struct STIView: View {
                 speed: 0.5,
                 blur: 0.7
             )
-            .saturation(1.1)
+            .saturation(1.3)
             .contrast(1.3)
-            .background(Color.white.opacity(colorScheme == .dark ? 0.1 : 0.7))
+            .background(Color.white.opacity(colorScheme == .dark ? 0.3 : 0.7))
             .ignoresSafeArea()
         )
         .clipShape(
@@ -280,5 +308,17 @@ struct STIView: View {
                     }
                 }
             }
+    }
+
+    private var circle: some View {
+        Circle()
+            .fill(Color.clear)
+            .frame(width: 30, height: 30)
+            .background(Blur(.systemUltraThinMaterialLight))
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .stroke(.white, lineWidth: 3)
+            )
     }
 }
